c2fbb3693f33ced9cfabd9cfea29fc77042399ac
nikalabs <ali@nikalabs.com>
2014-12-19 15:26:16 -0800

    update

---
+++ pending-specials.php
@@ -0,0 +1,101 @@
+<!doctype html>
+ <html>
+ <head>
+         <meta charset="utf-8">
+         <meta name="viewport" content="width=device-weidth, initial-scale=1.0">
+         <title>Front Apron</title>
+         <link href="css/bootstrap.min.css" rel="stylesheet">
+         <link href="css/custom.css" rel="stylesheet">
+         <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+         <script type="text/javascript">var switchTo5x=true;</script>
+         <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+         <script type="text/javascript">stLight.options({publisher: "735d4077-3253-4ad7-9752-27c59cd0b5e2", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+ </head>
+ <body>
+   
+<!-- Start of Main Body -->
+   <div class="main">
+    <div class="container">
+       <div class="page-header">
+        <h1>Pending Specials</h1>
+      </div>
+<div class="row"><!-- Start of the Row -->
+   
+       <div class="col-md-4"><!-- Start of the First Column -->
+        <div class="thumbnail">
+        <div class="image-container">
+                <div class="choose accept">
+                  <button class="btn btn-choose"><span class="glyphicon glyphicon-ok"></span></button>
+                </div>
+                <div class="choose cancel">
+                  <button class="btn btn-choose"><span class="glyphicon glyphicon-remove"></span></button>
+                </div>
+           
+           <img class="no-margin" src="images/thumbnail-1.png" alt="home-thumb">
+         </div>
+           <div class="caption">
+             <h3>Freshly brewed coffee<img class="company-logo" src="images/company-logo-starbucks.png" alt="star-bucks"></h3>
+                <p>Our coffee masters have distilled their years 
+of tasting knowledge down to three simple 
+questions to help you find a coffee you’re ...</p>
+ 
+              <h5><img src="images/location-pin.png"> San Jose <small>$10</small></h5>
+                               
+           </div>
+
+        </div>
+
+    </div><!-- End of the First Column -->
+            <div class="col-md-4"><!-- Start of the Second Column -->
+              <div class="thumbnail">
+                  <div class="image-container">
+                <div class="choose accept">
+                  <button class="btn btn-choose"><span class="glyphicon glyphicon-ok"></span></button>
+                </div>
+                <div class="choose cancel">
+                  <button class="btn btn-choose"><span class="glyphicon glyphicon-remove"></span></button>
+                </div>
+           <img class="no-margin" src="images/thumbnail-1.png" alt="home-thumb">
+         </div>
+           <div class="caption">
+             <h3>Freshly brewed coffee<img class="company-logo" src="images/company-logo-starbucks.png" alt="star-bucks"></h3>
+                <p>Our coffee masters have distilled their years 
+of tasting knowledge down to three simple 
+questions to help you find a coffee you’re ...</p>
+
+               <h5><img src="images/location-pin.png"> San Jose <small>$10</small></h5>
+           </div>
+          </div>
+      </div><!-- End of the Second Column -->
+        <div class="col-md-4"><!-- Start of the Third Column -->
+          <div class="thumbnail">
+              <div class="image-container">
+                <div class="choose accept">
+                  <button class="btn btn-choose"><span class="glyphicon glyphicon-ok"></span></button>
+                </div>
+                <div class="choose cancel">
+                  <button class="btn btn-choose"><span class="glyphicon glyphicon-remove"></span></button>
+                </div>
+           <img class="no-margin" src="images/thumbnail-1.png" alt="home-thumb">
+                    </div>
+           <div class="caption">
+             <h3>Freshly brewed coffee<img class="company-logo" src="images/company-logo-starbucks.png" alt="star-bucks"></h3>
+               <p>Our coffee masters have distilled their years 
+of tasting knowledge down to three simple 
+questions to help you find a coffee you’re ...</p>
+
+               <h5><img src="images/location-pin.png"> San Jose <small>$10</small></h5>
+           </div>
+          </div>
+        </div><!-- End of the Third Column -->
+   </div><!-- End of the Row -->
+     
+    </div><!-- End of the Container -->
+   
+   </div> <!-- End of the Main -->
+   
+
+ <script src="js/jquery.js"></script>
+ <script src="js/bootstrap.min.js"></script>
+ </body>
+ </html>
