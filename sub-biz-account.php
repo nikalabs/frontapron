aa5222737255893d7f1aabc16758d1566c4d5158
nikalabs <ali@nikalabs.com>
2014-12-22 08:48:22 -0800

    update

---
+++ sub-biz-account.php
@@ -0,0 +1,50 @@
+
+<!-- Start of Main Body -->
+   <div class="main">
+   	<div class="container">
+ <div class="row"><!-- Start of the Row -->
+ 
+  	      
+<div class="col-md-6"><!-- Start of the Column -->
+      <div class="page-header">
+        <h1>Sub-Biz Account (Store)</h1>
+      </div>
+      <form class="account-form" role="sign-up" action="mailto:youremail@email.com" method="post">
+        <div class="form-group">
+           <div class="username">Username<br/>
+          <input type="text" class="form-control user" placeholder="E-mail or Phone Number" name="user" id="user"></div>
+           <div class="password">Password<br/>
+          <input type="password" class="form-control pass" id="pass" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;" name="password" size="8" ></div>
+          <div class="again">Again<br/>
+          <input type="password" class="form-control pass" id="pass" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;" name="password" size="8" ></div>
+          <div class="first-name">First Name<br/>
+          <input type="text" class="form-control firts" placeholder="John" name="first" id="first"></div>
+          <div class="last-name">Last Name<br/>
+          <input type="text" class="form-control last" placeholder="Smith" name="last" id="last"></div>
+           <div class="businessname">Business Name<br/>
+          <input type="text" class="form-control business" placeholder="Enter Business Name" name="business" id="business"></div>
+           <div class="title-name">Title<br/>
+          <input type="text" class="form-control title" placeholder="Enter Title..." name="title" id="title"></div>
+          <div class="role">User Role<br/>
+          <select name="user_role"  id="user-role">
+  <option value="full-admin">Full Admin</option>
+  
+</select>
+</div>
+<div class="notification">Notification Sent to:<br/>
+          <input type="text" class="form-control notification" placeholder="JohnSmith@businessemail.com" name="notification" id="notification"></div>
+        </div>
+        
+        <button type="submit" class="btn btn-default update">Update</button>
+      </form>
+     
+     
+      
+    </div><!-- End of the Column -->
+          
+      </div><!-- End of the Row -->
+
+     
+    </div><!-- End of the Container -->
+   
+   </div> <!-- End of the Main -->
