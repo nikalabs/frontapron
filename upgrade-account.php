d8425de3f659f8a6b891f4c59842c4303ffb0d09
nikalabs <ali@nikalabs.com>
2014-12-22 11:21:43 -0800

    update

---
+++ upgrade-account.php
@@ -0,0 +1,65 @@
+
+<!-- Start of Main Body -->
+   <div class="main">
+   	<div class="container">
+ <div class="row"><!-- Start of the Row -->
+ 
+  	      
+<div class="col-md-4"><!-- Start of the Column -->
+      <div class="page-header">
+        <h1>Upgrade Account</h1>
+      </div>
+      <form class="account-form" role="sign-up" action="mailto:youremail@email.com" method="post">
+        <div class="form-group">
+          <div class="package">Package<br/>
+          <select name="package"  id="package">
+  <option value="1 item for $1.00 per day">1 item for $1.00 per day</option>
+  
+</select>
+</div>
+<table class="table" id="price-table">
+         <tr>
+    <th>1 Item</th>
+    <th></th>
+    <th class="last-cell">$1.00/day</th>
+  </tr>
+  <tr>
+    <th>100 Postings</th>
+    <th></th>
+    <th  class="last-cell">$29.99</th>
+  </tr>
+  </table>
+           <div class="coupon">Coupon Code<br/>
+          <input type="text" class="form-control coupon-code" placeholder="Enter Code..." name="coupon" id="coupon"></div>
+          <div class="card-name">Name on Card<br/>
+          <input type="text" class="form-control card-name" placeholder="John Smith" name="card_name" id="card-name"></div>
+          <div class="businessname">Business Name<br/>
+          <input type="text" class="form-control business-name" placeholder="Target Store 134" name="business_name" id="business-name"></div>
+           <div class="payment">Payment Method<br/>
+           <button type="submit" class="btn btn-default credit">Credit Card</button><button type="submit" class="btn btn-default paypal">paypal</button></div>
+          <div class="ccnumber">Credit Card Number<br/>
+          <input type="password" class="form-control ccnumber" id="ccnumber" placeholder="&#9679;&#9679;&#9679;&#9679; - &#9679;&#9679;&#9679;&#9679; - &#9679;&#9679;&#9679;&#9679; - 4392" name="credit_number" size="8" ></div>
+          <div class="cc-details">
+          <div class="exp-date">Expiration Date<br/>
+          <input type="text" class="form-control month" placeholder="11" name="exp_month" id="month"><input type="text" class="form-control year" placeholder="16" name="exp_year" id="year"></div>
+          <div class="cvv">CVV<br/>
+          <input type="text" class="form-control CVV-no" placeholder="493" name="CVV" id="CVV"></div>
+        </div>
+          <div class="ein">EIN #(for schools & Charities if applicable)<br/>
+          <input type="text" class="form-control ein-no" placeholder="Enter Ein #..." name="ein_number" id="ein"></div>
+        </div>
+        
+        <button type="submit" class="btn btn-default upgrade-account">Upgrade Account</button>
+      </form>
+     
+     
+      
+    </div><!-- End of the Column -->
+          
+      </div><!-- End of the Row -->
+
+     
+    </div><!-- End of the Container -->
+   
+   </div> <!-- End of the Main -->
+   
