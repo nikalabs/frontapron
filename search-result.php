0c0419031dbc596cd62d0782b4179d47e96bf112
nikalabs <ali@nikalabs.com>
2014-12-22 09:31:55 -0800

    update

---
+++ search-result.php
@@ -0,0 +1,94 @@
+
+<!-- Start of Main Body -->
+   <div class="main">
+   	<div class="container">
+
+   	  <div class="page-header">
+        <h5>2 Search Results Found Near San Jose</h5>
+      </div>
+      <div class="row"><!-- Start of the First Row -->
+  	   <div class="col-md-8"><!-- Start of the First Column -->
+        <div id="result">
+  	   <div class="sort">
+        <h4>Sort By</h4>
+        <p><a href="#">Friend's Specials</a></p>
+         <p><a href="#">Nearby Specials</a></p>
+          <p><a href="#">Ending Soon</a></p>
+       </div>
+       <div class="category">
+        <h4>Category</h4>
+        <div class="c-first">
+        <p><a href="#">Agriculture</a></p>
+         <p><a href="#">Accounting</a></p>
+          <p><a href="#">Advertising</a></p>
+        </div>
+         <div class="c-first">
+        <p><a href="#">Aerospace</a></p>
+         <p><a href="#">Aircraft</a></p>
+          <p><a class="more" href="#">more categories</a></p>
+        </div>
+       </div>
+     </div>
+
+     <table class="table" id="search-table">
+         
+  <tr>
+    <td class="pic"><img src="images/pic.png"></td>
+    <td class="descp"> <h3>Freshly brewed coffee</h3>
+                <p>Our coffee masters have distilled their years 
+of tasting knowledge down to three simple 
+questions to help you find a coffee you’re ...</p>
+</td>
+    <td> 
+      <div  class="details">
+      <h2>$10</h2>
+      <h5><img src="images/location-pin.png"> San Jose</h5>
+      <p><span class="glyphicon glyphicon-time"></span> 52min </p>
+    </div>
+    </td>
+  </tr>
+  <tr>
+   <td class="pic"><img src="images/pic.png"></td>
+    <td class="descp"> <h3>Freshly brewed coffee</h3>
+                <p>Our coffee masters have distilled their years 
+of tasting knowledge down to three simple 
+questions to help you find a coffee you’re ...</p>
+</td>
+    <td> 
+      <div  class="details">
+      <h2>$10</h2>
+      <h5><img src="images/location-pin.png"> San Jose</h5>
+      <p><span class="glyphicon glyphicon-time"></span> 52min </p>
+    </div>
+    </td>
+  </tr>
+   <tr>
+   <td class="pic"><img src="images/pic.png"></td>
+    <td class="descp"> <h3>Freshly brewed coffee</h3>
+                <p>Our coffee masters have distilled their years 
+of tasting knowledge down to three simple 
+questions to help you find a coffee you’re ...</p>
+</td>
+    <td> 
+      <div  class="details">
+      <h2>$10</h2>
+      <h5><img src="images/location-pin.png"> San Jose</h5>
+      <p><span class="glyphicon glyphicon-time"></span> 52min </p>
+    </div>
+    </td>
+  </tr>
+ 
+       </table>
+
+  	</div><!-- End of the  Column -->
+  	     
+
+     
+
+     
+      </div><!-- End of the  Row -->
+
+    </div><!-- End of the Container -->
+   
+   </div> <!-- End of the Main -->
+  
