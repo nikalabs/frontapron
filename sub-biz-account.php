8a5f43bdab08b2d8f4e02002362708a3ff1cc2cc
nikalabs <ali@nikalabs.com>
2014-12-23 16:05:15 -0800

    update

--- sub-biz-account.php
+++ sub-biz-account.php
@@ -12,17 +12,17 @@
       <form class="account-form" role="sign-up" action="mailto:youremail@email.com" method="post">
         <div class="form-group">
            <div class="username">Username<br/>
-          <input type="text" class="form-control user" placeholder="E-mail or Phone Number" name="user" id="user"></div>
+          <input type="text" class="form-control user" placeholder="E-mail or Phone Number" name="user_name" id="user"></div>
            <div class="password">Password<br/>
           <input type="password" class="form-control pass" id="pass" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;" name="password" size="8" ></div>
           <div class="again">Again<br/>
           <input type="password" class="form-control pass" id="pass" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;" name="password" size="8" ></div>
           <div class="first-name">First Name<br/>
-          <input type="text" class="form-control firts" placeholder="John" name="first" id="first"></div>
+          <input type="text" class="form-control firts" placeholder="John" name="first_name" id="first"></div>
           <div class="last-name">Last Name<br/>
-          <input type="text" class="form-control last" placeholder="Smith" name="last" id="last"></div>
+          <input type="text" class="form-control last" placeholder="Smith" name="last_name" id="last"></div>
            <div class="businessname">Business Name<br/>
-          <input type="text" class="form-control business" placeholder="Enter Business Name" name="business" id="business"></div>
+          <input type="text" class="form-control business" placeholder="Enter Business Name" name="business_name" id="business"></div>
            <div class="title-name">Title<br/>
           <input type="text" class="form-control title" placeholder="Enter Title..." name="title" id="title"></div>
           <div class="role">User Role<br/>
