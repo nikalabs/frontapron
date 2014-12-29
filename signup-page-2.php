2cc66dda425e164072844ae1f5b345377f3c3e68
nikalabs <ali@nikalabs.com>
2014-12-29 09:15:54 -0800

    update

---
+++ signup-page-2.php
@@ -0,0 +1,153 @@
+<!doctype html>
+ <html>
+ <head>
+      	 <meta charset="utf-8">
+      	 <meta name="viewport" content="width=device-weidth, initial-scale=1.0">
+      	 <title>Front Apron</title>
+      	 <link href="css/bootstrap.min.css" rel="stylesheet">
+      	 <link href="css/custom.css" rel="stylesheet">
+      	 <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+
+ </head>
+ <body>
+   <header>
+ 	 <div class="container">
+ 		<a class="navbar-brand" href="index.html"><img src="images/logo.png"></a>
+         <form class="head-signin-form " role="sign-in" action="mailto:youremail@email.com" method="post">
+        <div class="form-group">
+          <div class="phone">E-mail or Phone<br/>
+          <input type="text" class="form-control user" placeholder="JohnSmith4382@target.co" name="user"> </div>
+          <div class="pass">Password<br/>
+          <input type="password" class="form-control pass" id="pass" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;" name="password" size="8" >
+          <span><a href="#">forgot your password?</a></span></div>
+
+        </div>
+        <button type="submit" class="btn btn-default log">Log In</button>
+      </form>
+ 		
+ 	</div>
+   </header>
+ <!-- Start of Navigation Bar -->
+   <nav class="navbar navbar-default navbar-static-top" role="navigation">
+    <div class="container">
+    	 <div class="navbar-header">
+    	 	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-FA-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    	
+       </div>
+    <div class="collapse navbar-collapse" id="bs-FA-navbar-collapse-1">
+    	<ul class="nav navbar-nav">
+    		<li><a href="index.html">HOME</a></li>
+    		<li><a href="index.html">BOOKMARKS</a></li>
+    		<li><a href="index.html">MY SPECIALS</a></li>
+    		         
+    	</ul>
+    	<form class="navbar-form navbar-right" role="search">
+                <div class="form-group">
+                  <input type="text" class="form-control" placeholder="Coffee">
+                  <input type="text" class="form-control" placeholder="San Jose">
+                </div>
+                <button type="submit" class="btn btn-default search"><img src="images/search-icon.png"></button>
+        </form>
+      </div>
+
+    </div>
+   </nav>
+<!-- Start of Main Body -->
+   <div class="main">
+   	<div class="container">
+ <div class="row"><!-- Start of the First Row -->
+  <div class="col-md-6 first"><!-- Start of the First Column -->
+   	<div class="page-header">
+        <h1 style="color:transparent;">Sign Up</h1>
+      </div>  
+<p><strong>For Consumers:</strong><br/>
+Find ALL local New, Fresh, and On-promotion Specials from ALL local businesses daily anytime and anywhere!<br/>
+It is free and always will be!</p>
+
+<p><strong>For Local Businesses: </strong></br>
+Engage ALL local consumers with efficiency, effectiveness, and more revenue and profit generation daily.</p>
+ 
+<p><strong>For Schools or Charities:</strong></br>
+Sharing of FrontApron's 10% annual revenue by simply signing up charity members.</p>
+
+<p>Both Business and School/Charity simply Sign Up and then Upgrade.</p>
+     
+  	  
+  	</div><!-- End of the First Column -->
+  	      
+<div class="col-md-6 second"><!-- Start of the Second Column -->
+      <div class="page-header">
+        <h1>Sign Up</h1>
+      </div>
+      <form class="signup-form" role="sign-up" action="mailto:youremail@email.com" method="post">
+        <div class="form-group">
+          <p class="first-name">First Name<br/>
+          <input type="text" class="form-control firts" placeholder="John" name="first" id="first"></p>
+          <p class="last-name">Last Name<br/>
+          <input type="text" class="form-control last" placeholder="Smith" name="last" id="last"></p>
+           <p>E-mail or Mobile Number<br/>
+          <input type="text" class="form-control user" placeholder="JohnSmith4382@target.co" name="user"></p>
+          <p class="password">Password<br/>
+          <input type="password" class="form-control pass" id="pass" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;" name="password" size="8" ></p>
+          <p class="again">Again<br/>
+          <input type="password" class="form-control pass" id="pass" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;" name="password" size="8" ></p>
+          <span class="info">*Password must be strong and contain 1 letter, 1 number, and 1 special character.</span>
+        </div>
+        <button type="submit" class="btn btn-default sign-up">+ Create Account</button>
+      </form>
+       <div class="fb-log"><a href="#"><img src="images/fb-log.png"></a></div>
+     
+     
+      
+    </div><!-- End of the Second Column -->
+          
+      </div><!-- End of the First Row -->
+
+     
+    </div><!-- End of the Container -->
+   
+   </div> <!-- End of the Main -->
+   <!-- Start of the Footer -->
+   <footer class="footer">
+   	<div class="container">
+
+   	 <div class="row top">
+   	 	<div class="col-md-4">
+   	 		<ul class="footer-menu">
+   	 			<li><a href="#">About Front Apron</a></li>
+   	 			<li><a href="#">Businesses</a></li>
+   	 			<li><a href="#">Terms & Privacy</a></li>
+   	 			<li><a href="#">Blog</a></li>
+   	 			<li><a href="#">Help</a></li>
+   	 		</ul>
+   	 	</div><!-- End of the Footer First Column -->
+   	 	<div class="col-md-4">
+   	 	</div><!-- End of the Footer Second Column -->
+   	 	<div class="col-md-4">
+   	 	 <div class="footer-icons">
+   	 	  <a class="google-play"href="#"><img src="images/google-play.png"></a>
+   	 	  <a class="appstore"href="#"><img src="images/appstore.png"></a>
+   	 	 </div>
+   	 	 <div class="footer-socials">
+   	 	 	<a class="fbook"><img src="images/facebook.png"></a>
+   	 	 	<a class="twitter"><img src="images/twitter.png"></a>
+   	 	 	<a class="gplus"><img src="images/googleplus.png"></a>
+   	 	 </div>
+   	 	</div><!-- End of the Footer Third Column -->
+   	 </div><!-- End of the Footer Row Top -->
+   	 <div class="row bottom">
+   	 	<p class="copy-right">© 2014, Front Apron.  All Rights Reserved.</p>
+   	 </div>
+   	</div><!-- End of the Footer Container -->
+   </footer>
+   <!-- End of the Footer -->
+
+ <script src="js/jquery.js"></script>
+ <script src="js/bootstrap.min.js"></script>
+ </body>
+ </html>
